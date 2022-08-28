3c15d2e77b3d19ac7fd9f04d80a5149be34f62a3
ryancrideout <ryancrideout@gmail.com>
2022-08-27 20:45:30 -0600

    Lots of migration changes.

--- aoe2_stats/populate_database.py
+++ aoe2_stats/populate_database.py
@@ -13,22 +13,27 @@ def main():
     # 1640995200 - January 1st, 2022
 
     # Most recent timestamp -
-    # 1596514493
-    # August 4th, 2020
+    # 1596994958
+    # August 9th, 2020
+
     print("Starting the script.")
-    api_client.get_all_matches_since_time(since=1596514493, until=1640995200)
+    api_client.get_all_matches_since_time(since=1596994958, until=1640995200)
     print("Script complete!")
 
     # Savepoint:
     """
-    Match 6baf388f-b7b2-7246-a391-711feb10c1b4 has been saved.
-    Player 2688268 already exists!
-    PlayerMatch [Match: 6baf388f-b7b2-7246-a391-711feb10c1b4 - Player: 2688268] has been saved.
-    Player 270492 has been saved.
-    PlayerMatch [Match: 6baf388f-b7b2-7246-a391-711feb10c1b4 - Player: 270492] has been saved.
-    Player 2686183 already exists!
-    PlayerMatch [Match: 6baf388f-b7b2-7246-a391-711feb10c1b4 - Player: 2686183] has been saved.
-    Player 1094730 has been saved.
-    PlayerMatch [Match: 6baf388f-b7b2-7246-a391-711feb10c1b4 - Player: 1094730] has been saved.
+    Match 461766c6-a7ce-2045-856a-ad1c40686bee has been saved.
+    Player 2589306 already exists!
+    PlayerMatch [Match: 461766c6-a7ce-2045-856a-ad1c40686bee - Player: 2589306] has been saved.
+    Player 2898768 already exists!
+    PlayerMatch [Match: 461766c6-a7ce-2045-856a-ad1c40686bee - Player: 2898768] has been saved.
+    Player 2209305 already exists!
+    PlayerMatch [Match: 461766c6-a7ce-2045-856a-ad1c40686bee - Player: 2209305] has been saved.
+    Player 2155445 already exists!
+    PlayerMatch [Match: 461766c6-a7ce-2045-856a-ad1c40686bee - Player: 2155445] has been saved.
+    Player 252391 already exists!
+    PlayerMatch [Match: 461766c6-a7ce-2045-856a-ad1c40686bee - Player: 252391] has been saved.
+    Player 3031980 has been saved.
+    PlayerMatch [Match: 461766c6-a7ce-2045-856a-ad1c40686bee - Player: 3031980] has been saved.
     """
 main()
