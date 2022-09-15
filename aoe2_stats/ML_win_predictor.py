d5c5e94c6e807a0e52a576a4300c3523d09e396a
ryancrideout <ryancrideout@gmail.com>
2022-09-15 16:44:35 -0600

    Saving work, this is ready to go just need to wait until the site is up.

---
+++ aoe2_stats/ML_win_predictor.py
@@ -0,0 +1,20 @@
+from aoe2netapi import AOE2NETAPI
+
+"""
+Okay what do I want to filter by?
+
+Trying to figure out "won".
+
+Can feed the algorithm:
+Player Match Data
+1) civ
+2) rating
+3) color
+
+Match Data:
+1) map
+2) started
+3) (finished - started) = length
+
+Don't we need to compare the data against whoever is playing?
+"""
--- aoe2_stats/populate_database.py
+++ aoe2_stats/populate_database.py
@@ -13,32 +13,28 @@ def main():
     # 1640995200 - January 1st, 2022
 
     # Most recent timestamp -
-    # 1598642973
-    # August 28th, 2020
+    # 1599429793
+    # September 6, 2020
 
     print("Starting the script.")
     # TODO: Take the information from the savepoint and update the most recent timestamp + api_client.
-    api_client.get_all_matches_since_time(since=1598642973, until=1640995200)
+    api_client.get_all_matches_since_time(since=1599429793, until=1640995200)
     print("Script complete!")
 
     # Savepoint:
     """
-    Match b405b003-7e8a-114b-aca1-e987341619ec has been saved.
-    Player 2413814 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 2413814] has been saved.
-    Player 2753748 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 2753748] has been saved.
-    Player 2023988 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 2023988] has been saved.
-    Player 1986425 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 1986425] has been saved.
-    Player 2908959 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 2908959] has been saved.
-    Player 3317419 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 3317419] has been saved.
-    Player 3185575 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 3185575] has been saved.
-    Player 1898052 already exists!
-    PlayerMatch [Match: b405b003-7e8a-114b-aca1-e987341619ec - Player: 1898052] has been saved.
+    Match 271df540-a3e2-1145-bfc5-0d53adef5451 has been saved.
+    Player 2762511 already exists!
+    PlayerMatch [Match: 271df540-a3e2-1145-bfc5-0d53adef5451 - Player: 2762511] has been saved.
+    Player 428417 already exists!
+    PlayerMatch [Match: 271df540-a3e2-1145-bfc5-0d53adef5451 - Player: 428417] has been saved.
+    Player 2768175 already exists!
+    PlayerMatch [Match: 271df540-a3e2-1145-bfc5-0d53adef5451 - Player: 2768175] has been saved.
+    Player 2300252 already exists!
+    PlayerMatch [Match: 271df540-a3e2-1145-bfc5-0d53adef5451 - Player: 2300252] has been saved.
+    Player 654714 already exists!
+    PlayerMatch [Match: 271df540-a3e2-1145-bfc5-0d53adef5451 - Player: 654714] has been saved.
+    Player 1050058 already exists!
+    PlayerMatch [Match: 271df540-a3e2-1145-bfc5-0d53adef5451 - Player: 1050058] has been saved.
     """
 main()
