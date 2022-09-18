795058869a29c504f4b6c00a61e1b32532d4bca8
ryancrideout <ryancrideout@gmail.com>
2022-09-18 14:24:13 -0600

    Saving my work. Then some more rest.

--- aoe2_stats/ML_win_predictor.py
+++ aoe2_stats/ML_win_predictor.py
@@ -18,3 +18,35 @@ Match Data:
 
 Don't we need to compare the data against whoever is playing?
 """
+
+"""
+First order of business is to make a data frame... so we need the data from above and figure out a format for it.
+
+This will help:
+https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.from_dict.html
+"""
+import pandas as pd
+from aoe2net_django.wsgi import *
+from aoe2net_database.models import Player, Match
+from aoe2netapi import AOE2NETAPI
+
+dataframe_dict = {
+    "player_one_civ": [],
+    "player_one_rating": [],
+    "player_one_color": [],
+    "player_one_victory": [],
+    "map": [],
+    "time_started": [],
+    "game_length": [],
+    "player_two_civ": [],
+    "player_two_rating": [],
+    "player_two_color": [],
+    "player_two_victory": [],
+}
+
+ranked_1v1_matches = Match.objects.all().filter(game_type=0, rating_type=2) 
+ranked_1v1_match_ids = [match.id for match in ranked_1v1_matches]
+
+"""
+For match in match... collect data?
+"""
