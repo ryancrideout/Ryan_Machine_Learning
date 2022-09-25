1a99d77e6af62831976b54a5bec10d7b295f0772
ryancrideout <ryancrideout@gmail.com>
2022-09-25 14:22:48 -0600

    Alright finished up this script for now, but will need to revisit it I think?

--- aoe2_stats/ML_win_predictor.py
+++ aoe2_stats/ML_win_predictor.py
@@ -1,5 +1,3 @@
-from aoe2netapi import AOE2NETAPI
-
 """
 Okay what do I want to filter by?
 
@@ -40,23 +38,23 @@ https://www.reddit.com/user/dj0wns/
 import pandas as pd
 from aoe2net_django.wsgi import *
 from aoe2net_database.models import Match
-from aoe2netapi import AOE2NETAPI
+from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 dataframe_dict = {
-    "player_one_civ": [],
-    "player_one_rating": [],
-    "player_one_color": [],
+    # "player_one_civ": [],
+    # "player_one_rating": [],
+    # "player_one_color": [],
     "player_one_victory": [],
-    "map": [],
-    "time_started": [],
-    "game_length": [],
-    "player_two_civ": [],
-    "player_two_rating": [],
-    "player_two_color": [],
-    "player_two_victory": [],
+    # "map": [],
+    # "time_started": [],
+    # "game_length": [],
+    # "player_two_civ": [],
+    "player_two_rating_difference": [],
+    # "player_two_color": [],
+    # "player_two_victory": [],
 }
 
 ranked_1v1_matches = Match.objects.all().filter(game_type=0, rating_type=2) 
@@ -68,26 +66,45 @@ matches_count = len(ranked_1v1_matches)
 
 """
 For match in match... collect data?
+
+I think for the ratings, I need to take the average of the two,
+and then check how much lower/higher they are from the average.
 """
 for match in ranked_1v1_matches:
-    dataframe_dict["map"].append(match.map)
-    dataframe_dict["time_started"].append(match.started)
+    player_one = match.players.all()[0]
+    player_two = match.players.all()[1]
+    # Lazy safety checks because I'm lazy.
+    if not player_one.rating:
+        continue
+    if not player_two.rating:
+        continue
+    if not player_one.color:
+        continue
+    if not player_two.color:
+        continue
+    if not match.started:
+        continue
+    if not match.finished:
+        continue
+    # dataframe_dict["map"].append(match.map)
+    # dataframe_dict["time_started"].append(match.started)
     length = match.finished - match.started
-    dataframe_dict["game_length"].append(length)
-    player_one = match.players[0]
-    player_two = match.players[1]
+    # dataframe_dict["game_length"].append(length)
 
     # We need a color map - 1 is red, 2 is blue, etc.
-    dataframe_dict["player_one_civ"].append(player_one.civ)
-    dataframe_dict["player_one_rating"].append(player_one.rating)
-    dataframe_dict["player_one_color"].append(player_one.color)
+    # dataframe_dict["player_one_civ"].append(player_one.civ) # Hmm... these strings seem to cause problems, re-evaluate.
+    # dataframe_dict["player_one_rating"].append(player_one.rating)
+    # dataframe_dict["player_one_color"].append(player_one.color)
     # I think this will be our "Y" value.
-    dataframe_dict["player_one_victory"].append(player_one.won)
+    if player_one.won:
+        dataframe_dict["player_one_victory"].append("Victory")
+    else:
+        dataframe_dict["player_one_victory"].append("Defeat")
 
-    dataframe_dict["player_two_civ"].append(player_two.civ)
-    dataframe_dict["player_two_rating"].append(player_two.rating)
-    dataframe_dict["player_two_color"].append(player_two.color)
-    dataframe_dict["player_two_victory"].append(player_two.won)
+    # dataframe_dict["player_two_civ"].append(player_two.civ)
+    dataframe_dict["player_two_rating_difference"].append(player_one.rating - player_two.rating)
+    # dataframe_dict["player_two_color"].append(player_two.color)
+    # dataframe_dict["player_two_victory"].append(player_two.won)
 
 # Make the dataframe. We're going to split this up into X and Y components.
 dataframe = pd.DataFrame.from_dict(dataframe_dict)
@@ -98,14 +115,33 @@ Y = dataframe["player_one_victory"]
 # Make our model and our training data.
 model = DecisionTreeClassifier()
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+# print("What is X_train and Y_train?") # 3788 data points!
+# print(X_train)
+# print(Y_train) 
 model.fit(X_train, Y_train)
 predictions = model.predict(X_test)
 
 score = accuracy_score(Y_test, predictions)
 # Be sure to print out the score!
+print("We finally got this point... we have the score!")
+print(score)
+
+# print("What is the Y.unique()?")
+# print(Y.unique()) # [False  True]
 
 # I think once we have the predictions (and have trained the model), then we can put it into a decision tree
-# 
+tree.export_graphviz(
+    model, 
+    out_file='aoeII_win_predictor_EXTRA_LEAN.dot',
+    feature_names=[
+        "player_two_rating_difference",
+        ], 
+    class_names=sorted(Y.unique()),
+    label='all',
+    rounded=True,
+    filled=True
+)
+print("AND WE ARE DONE!")
 """
 Once we have the data we machine learn it?
 """
@@ -121,4 +157,17 @@ tree.export_graphviz(
     rounded=True,
     filled=True
 )
+"""
+"""
+dataframe_dict = {
+    "player_one_rating": [],
+    "player_one_color": [],
+
+    "time_started": [],
+    "game_length": [],
+
+    "player_two_rating": [],
+    "player_two_color": [],
+    "player_two_victory": [],
+}
 """
